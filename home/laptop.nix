aaf540c263c6e929bd962846cc3fbecdf579046f
Leonard Niedens <niedens03@gmail.com>
2026-04-13 21:35:31 +0200

    fix kwin latency persistence and fusuma interval (2026-04-13 21:35)

--- home/laptop.nix
+++ home/laptop.nix
@@ -1,13 +1,23 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
+  # KWin-Latenz und Animationsgeschwindigkeit optimieren
+  home.activation.kwinLatencyFix = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    $DRY_RUN_CMD ${pkgs.kdePackages.kconfig}/bin/kwriteconfig6 \
+      --file kdeglobals --group KDE --key AnimationDurationFactor 0.5
+    $DRY_RUN_CMD ${pkgs.kdePackages.kconfig}/bin/kwriteconfig6 \
+      --file kwinrc --group Compositing --key LatencyPolicy Low
+    $DRY_RUN_CMD ${pkgs.kdePackages.kconfig}/bin/kwriteconfig6 \
+      --file kwinrc --group TabBox --key DelayTime 0
+  '';
+
   # Fusuma: Touchpad-Gesten für Wayland/KDE Plasma 6
   services.fusuma = {
     enable = true;
     extraPackages = with pkgs; [ coreutils qt6.qttools ];
     settings = {
       threshold = { swipe = 0.05; };
-      interval = { swipe = 0; };
+      interval = { swipe = 1; };
       swipe = {
         "3" = {
           up.command    = "qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.Component.invokeShortcut 'Overview'";
--- system/laptop.nix
+++ system/laptop.nix
@@ -21,9 +21,13 @@
 
   boot.kernelModules = [ "uinput" ];
 
-  # AMD Renoir GFXOFF deaktivieren – verhindert GPU-Wakeup-Delay bei KWin-Effekten
-  # (Alt+Tab, Super+W etc. sonst ~1s verzögert weil GPU aufwachen muss)
+  # AMD Renoir: GFXOFF deaktivieren + GPU auf performance halten
+  # Verhindert ~1s Wakeup-Delay bei KWin-Effekten (Alt+Tab, Overview etc.)
   boot.kernelParams = [ "amdgpu.gfxoff=0" ];
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="drm", KERNEL=="card[0-9]", DRIVERS=="amdgpu", \
+      ATTR{device/power_dpm_force_performance_level}="high"
+  '';
 
   # Touchpad
   services.libinput = {
