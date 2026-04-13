c37d684cdb65799f431b07093d915eeb07bd2440
Leonard Niedens <niedens03@gmail.com>
2026-04-13 22:23:48 +0200

    kanata: fix key roll order with process-unmapped-keys (2026-04-13 22:23)

--- system/laptop.nix
+++ system/laptop.nix
@@ -44,6 +44,10 @@
     keyboards.default = {
       devices = [ "/dev/input/by-path/platform-i8042-serio-0-event-kbd" ];
       config = ''
+      (defcfg
+        process-unmapped-keys yes
+      )
+
       (defsrc
         a s d f j k l '
       )
