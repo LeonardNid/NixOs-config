6a90d966f579dee392958569a0e8e66d1328f10d
Leonard Niedens <niedens03@gmail.com>
2026-04-13 21:19:09 +0200

    gestures: add 3-finger left/right for workspace switching (2026-04-13 21:19)

--- home/laptop.nix
+++ home/laptop.nix
@@ -10,8 +10,10 @@
       interval = { swipe = 0; };
       swipe = {
         "3" = {
-          up.command   = "qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.Component.invokeShortcut 'Overview'";
-          down.command = "qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.Component.invokeShortcut 'Show Desktop'";
+          up.command    = "qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.Component.invokeShortcut 'Overview'";
+          down.command  = "qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.Component.invokeShortcut 'Show Desktop'";
+          left.command  = "qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.Component.invokeShortcut 'Switch to Next Desktop'";
+          right.command = "qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.Component.invokeShortcut 'Switch to Previous Desktop'";
         };
         "4" = {
           left.command  = "qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.Component.invokeShortcut 'Switch to Next Desktop'";
