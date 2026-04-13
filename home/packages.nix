da9329e818b1d5865d949670173651cf365e056d
Leonard Niedens <niedens03@gmail.com>
2026-04-13 22:46:50 +0200

    fix obsidian taskbar name: run natively on wayland (2026-04-13 22:46)

--- home/packages.nix
+++ home/packages.nix
@@ -6,4 +6,14 @@
     keymapp
     libnotify
   ];
+
+  # Obsidian nativ unter Wayland starten – verhindert "electron"-Anzeige in der Taskleiste
+  xdg.desktopEntries.obsidian = {
+    name = "Obsidian";
+    exec = "obsidian --ozone-platform=wayland %u";
+    icon = "obsidian";
+    categories = [ "Office" ];
+    mimeType = [ "x-scheme-handler/obsidian" ];
+    comment = "A powerful knowledge base";
+  };
 }
