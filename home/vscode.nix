d92d9c3330e8623440e7ccc9f6c2b6bda0fd5e47
Leonard Niedens <niedens03@gmail.com>
2026-04-16 18:56:06 +0200

    vscode wallet (2026-04-16 18:56)

--- home/vscode.nix
+++ home/vscode.nix
@@ -3,6 +3,7 @@
 {
   programs.vscode = {
     enable = true;
+    package = pkgs.vscode.override { commandLineArgs = "--password-store=kwallet6"; };
     profiles.default = {
       extensions = with pkgs.vscode-extensions; [
         github.copilot
