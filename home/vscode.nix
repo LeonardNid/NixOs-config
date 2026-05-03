e756b98e0b39647e74d339f884d13c6065054675
Leonard Niedens <niedens03@gmail.com>
2026-05-03 14:12:54 +0200

    vsc (2026-05-03 14:12)

--- home/vscode.nix
+++ home/vscode.nix
@@ -8,7 +8,15 @@
       extensions = with pkgs.vscode-extensions; [
         github.copilot
         github.copilot-chat
+        vscodevim.vim
       ];
+      userSettings = {
+        "editor.lineNumbers" = "relative";
+        "vim.useSystemClipboard" = true;
+        "vim.handleKeys" = {
+          "<C-f>" = false;
+        };
+      };
     };
   };
 }
