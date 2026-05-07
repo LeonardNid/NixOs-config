d0b6603be1b257c523e7dd78b2ca40813c22bc8a
Leonard Niedens <niedens03@gmail.com>
2026-05-07 08:23:53 +0200

    update (2026-05-07 08:23)

--- home/shell.nix
+++ home/shell.nix
@@ -15,6 +15,15 @@
     };
     initContent = ''
       eval "$(zoxide init zsh)"
+
+      function y() {
+        local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+        yazi "$@" --cwd-file="$tmp"
+        if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+          builtin cd -- "$cwd"
+        fi
+        rm -f -- "$tmp"
+      }
     '';
   };
 
